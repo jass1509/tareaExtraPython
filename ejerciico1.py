5b383f1fae07c450466b600547136d9f1cbf7234
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-12-15 17:53:35 -0500

    ejerciio 1 terminado

---
+++ ejerciico1.py
@@ -0,0 +1,4 @@
+nombre_usuario=input("Ingrese su nombre: ")
+dominio=input("Ingrese el dominio de su correo ")
+correo_electronico= nombre_usuario + "@" + dominio
+print("Su correo electronico es: ", correo_electronico)
