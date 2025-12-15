576660a3e208f1dbe8de5481b5446485b28dbfdb
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-12-15 18:14:02 -0500

    sacando al min y max fuera de la iteracion para que no se reinciien con cada pasada

--- ejerciio2.py
+++ ejerciio2.py
@@ -5,13 +5,14 @@ nota4=input("ingrese la cuarta nota ")
 suma_notas= float(nota1) + float(nota2) + float(nota3) + float(nota4)
 lista_notas= [float(nota1), float(nota2), float(nota3), float(nota4)]
 
+minimo=lista_notas[0]
+maximo=lista_notas[0]
+
 for elementos in lista_notas:
-    minimo=lista_notas[0]
     if elementos<minimo:
         minimo=elementos
 for elementos in lista_notas:
-    maximo=lista_notas[0]
     if elementos>maximo:
         maximo=elementos
 nota_final= (suma_notas - minimo + maximo) / 4
-print("la nota final es"+ nota_final)
+print("la nota final es", nota_final)
