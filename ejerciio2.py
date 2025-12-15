036755231659fc6a160d231f12a4512b09c4545f
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-12-15 18:00:09 -0500

    identificando la mayor y menor nota

--- ejerciio2.py
+++ ejerciio2.py
@@ -2,3 +2,8 @@ nota1=input("ingrese la primera nota ")
 nota2=input("ingrese la segunda nota ")
 nota3=input("ingrese la tercera nota ")
 nota4=input("ingrese la cuarta nota ")
+
+menor_nota= min(nota1, nota2, nota3, nota4)
+print("la menor nota: ", menor_nota)
+nota_mayor= max(nota1, nota2, nota3, nota4)
+print("la mayor nota: ", nota_mayor)
