7be6ef871c58adfbc88c146c3aaddb3b942df79d
jass1509 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2025-12-15 18:10:38 -0500

    usnado listas e funciones iterativas para encontrar al nro mayo

--- ejerciio2.py
+++ ejerciio2.py
@@ -2,8 +2,16 @@ nota1=input("ingrese la primera nota ")
 nota2=input("ingrese la segunda nota ")
 nota3=input("ingrese la tercera nota ")
 nota4=input("ingrese la cuarta nota ")
+suma_notas= float(nota1) + float(nota2) + float(nota3) + float(nota4)
+lista_notas= [float(nota1), float(nota2), float(nota3), float(nota4)]
 
-menor_nota= min(nota1, nota2, nota3, nota4)
-print("la menor nota: ", menor_nota)
-nota_mayor= max(nota1, nota2, nota3, nota4)
-print("la mayor nota: ", nota_mayor)
+for elementos in lista_notas:
+    minimo=lista_notas[0]
+    if elementos<minimo:
+        minimo=elementos
+for elementos in lista_notas:
+    maximo=lista_notas[0]
+    if elementos>maximo:
+        maximo=elementos
+nota_final= (suma_notas - minimo + maximo) / 4
+print("la nota final es"+ nota_final)
